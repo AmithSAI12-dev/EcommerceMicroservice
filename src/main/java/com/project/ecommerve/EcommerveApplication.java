6761ddcaada36835db4200f80057ebe0081a7f15
Amith Sai <amithsai78@gmail.com>
2021-05-04 16:16:04 +0530

    Initial Commit

---
+++ src/main/java/com/project/ecommerve/EcommerveApplication.java
@@ -0,0 +1,13 @@
+package com.project.ecommerve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerveApplication.class, args);
+	}
+
+}
